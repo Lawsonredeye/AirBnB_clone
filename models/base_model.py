41f23629b1afaf7ed4106f30cc8d612ef124494e
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-07 02:05:48 +0100

    updated the basemodel to take instanciate the other models that would be created

--- models/base_model.py
+++ models/base_model.py
@@ -3,3 +3,25 @@
 Module for parent class which would act as the base
 class for inheriting sub-classes
 """
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at']= self.created_at.isoformat()
+        dict_copy['updated_at']= self.updated_at.isoformat()
+        return dict_copy
+
+    def __str__(self):
+        return (f"[BaseModel] ({self.id}) {self.__dict__}")
