1f0dad2234fa6b8081d784c44e26e71b47b284f4
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-07 02:07:47 +0100

    fixed pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ class for inheriting sub-classes
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
@@ -19,8 +20,8 @@ class BaseModel:
     def to_dict(self):
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at']= self.created_at.isoformat()
-        dict_copy['updated_at']= self.updated_at.isoformat()
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
         return dict_copy
 
     def __str__(self):
