13048ee4b87f0e24244e330289c9618eb76e89f0
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-10 20:02:56 +0100

    modified the entire file cose errors was found during the point of merging and old commits were mistakenly pushed but its rectified now

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,93 +4,28 @@ class module that serves as a storage for our
 airbnb web application which can be called at
 any time using json
 """
-<<<<<<< HEAD
-# import json
-# from models.base_model import BaseModel
-=======
-import json
-from models.base_model import BaseModel
->>>>>>> e70b71093f124b9db7d22107e9803983c4397c9e
-
-# class FileStorage:
-#     """
-#     class storage to store all instances that would be created
-#     for all instances
-
-#     Args:
-#         __file_path : path to file
-#         __object: dictionary for storing key and values
-#     """
-#     __file_path = "file.json"
-#     __objects = {}
-
-#     def all(self):
-#         """
-#         returns the dictionary values like a getter
-#         """
-#         return self.__objects
-
-#     def new(self, obj):
-#         """
-#         Creates/ instanciate the class object name with the class object
-#         id and then pass the value (obj) into the key of the __object dict
-
-#         Args:
-#             obj: value to be added to the __object attribute
-#         """
-#         key = f"{obj.__class__.__name__ }.{str(obj.id)}"
-#         self.__objects[key] = obj
-#         return self.__objects
-
-#     def save(self):
-#         """
-#         Saves all instances created into a json format and
-#         dumps into a file which it can be called from when needed.
-#         Creates a file if it doesnt exist
-#         """
-#         # from ..base_model import BaseModel
-#         new_objects = {key: value.to_dict() for key, value in self.__objects.items()}
-#         with open(self.__file_path, "w", encoding="utf-8") as f:
-#             json.dump(new_objects, f)
-
-#     def reload(self):
-#         """
-#         Deserialize the JSON string inside of the __objects attribute
-#         and then doesnt pass an error if the file is not in existence
-#         """
-#         if self.__file_path is not None:
-#             try:
-#                 with open(self.__file_path, "r", encoding="utf-8") as f:
-#                     # self.__objects = json.load(f)
-#                     new_obj = json.load(f)
-#                     for i, j in new_obj.items():
-#                         class_name, class_id = i.split('.')
-#                         new_dict = globals()[class_name](**j)
-#                         self.__objects = new_obj
-#             except FileNotFoundError:
-#                 pass
-
 import json
 from models.base_model import BaseModel
 
 class FileStorage:
-    __file_storage = 'file.json'
+    """
+    class storage to store all instances that would be created
+    for all instances
+
+    Args:
+        __file_path : path to file
+        __object: dictionary for storing key and values
+    """
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """
+        returns the dictionary values like a getter
+        """
         return self.__objects
-    
-    def new(self, obj):
-<<<<<<< HEAD
-        key = f"{type(obj).__name__}.{obj.id}"
-        self.__objects[key] = obj
 
-    def save(self):
-        new_object = {key: value.to_dict for key, value in self.__objects}
-        with open(self.__file_storage, 'w') as f:
-            json.dump(new_object, f)
-    
-=======
+    def new(self, obj):
         """
         Creates/ instanciate the class object name with the class object
         id and then pass the value (obj) into the key of the __object dict
@@ -107,26 +42,20 @@ class FileStorage:
         Saves all instances created into a json format and
         dumps into a file which it can be called from when needed.
         Creates a file if it doesnt exist
-
-        new dictionary was created which would store the __objects
-        values and then be passed onto the dump for a json representation
         """
         # from ..base_model import BaseModel
-        # from models.base_model import BaseModel
+        new_objects = {key: value.to_dict() for key, value in self.__objects.items()}
+        with open(f"{self.__file_path}", "w", encoding="utf-8") as f:
+            json.dump(new_objects, f)
 
-        new = {key: value.to_dict() for key, value in self.__objects.items()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(new, f)
-
->>>>>>> e70b71093f124b9db7d22107e9803983c4397c9e
     def reload(self):
-        if self.__file_storage is not None:
+        """
+        Deserialize the JSON string inside of the __objects attribute
+        and then doesnt pass an error if the file is not in existence
+        """
+        if self.__file_path is not None:
             try:
-<<<<<<< HEAD
-                with open(self.__file_storage, "r", encoding="utf-8") as f:
-=======
                 with open(self.__file_path, "r", encoding="utf-8") as f:
->>>>>>> e70b71093f124b9db7d22107e9803983c4397c9e
                     data_dict = json.load(f)
                     for key, value in data_dict.items():
                         class_name, instance_id = key.split('.')
